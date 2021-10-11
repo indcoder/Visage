cebd2cdea22e201002745973465fb86ab0b98bfa
Augustine Correa <augcor@gmail.com>
2021-10-12 00:33:19 +0530

    fix: :bug: GH action failure caused by missing default index file
    Because of the comment in gitignore and coming from a JS background, the uncommenting of wwwroot ensued that the folder and it content were no more part of git artifacts. But this is required by Blazor during its build process. Fixes #11
    
    #11

--- web/Program.cs
+++ web/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Azure.Functions.Authentication.WebAssembly;
 using web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -8,6 +7,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddStaticWebAppsAuthentication();
 
 await builder.Build().RunAsync();
