4ec2673aa2db602c32e7fb51e1ea143cb8458fa1
Augustine Correa <augcor@gmail.com>
2021-12-31 00:51:48 +0530

    feat: :sparkles: Process Roles from Auth0 payload
    We are checking if the User Profile receieved from Auth0 contains roles and if so return them when this function is invoked

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -1,10 +1,12 @@
 module.exports = async function (context, req) {
+    const user = req.body || {};
+
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const roles = ['weathercaster', 'dummy'];
-    
-    console.log({roles});
+    const roles = user["https://dev-visage.in/roles"] || [];
 
+
+    
     context.res.json({
         roles
     });
