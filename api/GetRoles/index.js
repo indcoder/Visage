593f6eb8b4a1ecffe0e6389395176ffb82c678a7
Augustine Correa <augcor@gmail.com>
2022-07-31 21:51:15 +0530

    Update index.js

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -1,6 +1,10 @@
 module.exports = async function (context, req) {
     const user = req.body || {};
-
+    
+    console.log("The user is ");
+    console.log({user});
+    console.log("End of user");
+    
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const roles = user["https://dev-visage.in/roles"] || [];
