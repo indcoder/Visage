2d54e3a45c1e64374821248cef58d857ab162cad
Augustine Correa <augcor@gmail.com>
2022-08-15 19:22:30 +0530

    fix: :bug: Extract claims from Auth0

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -1,12 +1,26 @@
+
 module.exports = async function (context, req) {
     const user = req.body || {};
-
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const roles = user["https://dev-visage.in/roles"] || [];
+    console.log("The user is ");
+    console.log({user});
+    console.log("End of user");
+    context.log({user});
+    console.log('JavaScript HTTP trigger function processed a request.');
+    const claims = user.claims || [];
+    const roles = [];
 
 
     
+    for(i=0; i < claims.length; i++) {
+        if(claims[i].typ === "https://dev-visage.in/roles") {
+            roles.push(claims[i].val);
+        }
+    }
+
+    /* Iterate through the claims array and push the value to roles*/
+
+
+  
     context.res.json({
         roles
     });
