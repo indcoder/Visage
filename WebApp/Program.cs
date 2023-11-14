f30d2bf405b36ea3fe07a3ea8251d0a181fb66a3
Augustine Correa <augcor@gmail.com>
2023-11-14 18:06:36 +0530

    Initial Home Page functionality with Login

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -9,11 +9,18 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddOptions();
+
 builder.Services.AddOidcAuthentication(options =>
 {
     // Configure your authentication provider options here.
     // For more information, see https://aka.ms/blazor-standalone-auth
-    builder.Configuration.Bind("Local", options.ProviderOptions);
+    builder.Configuration.Bind("hackmum", options.ProviderOptions);
 });
+
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
+
+builder.Services.AddAuthorizationCore();
+
 
 await builder.Build().RunAsync();
